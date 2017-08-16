9009a55d24d4774e1774f22bc14fd69bf1246f4e
Renato C. Alves <rcardosoalves@outlook.com>
2017-08-16 18:58:12 -0300

    Aula 06

--- src/main/java/br/com/fabricadeprogramador/servlet/ClienteServlet.java
+++ src/main/java/br/com/fabricadeprogramador/servlet/ClienteServlet.java
@@ -17,6 +17,28 @@ public class ClienteServlet extends HttpServlet {
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	public ClienteServlet() {
+		System.out.println("Contruindo o servlet...");
+	}
+	
+	@Override
+	public void init() throws ServletException {
+		System.out.println("Inicializando o servlet...");
+		super.init();
+	}
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Chamou o método service...");
+		super.service(request, response);
+	}
+	
+	@Override
+	public void destroy() {
+		System.out.println("Destruindo o servlet...");
+		super.destroy();
+	}
+	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Chamou pelo método GET.");
